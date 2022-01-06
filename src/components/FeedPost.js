ee5a40713e0cd0b0a6c22d5660c08680b199dcd8
Ian Powe <ian.powe@morehouse.edu>
2022-01-06 12:49:10 -0500

    Changes for Feed
    Added files for the posts that show up on the feed. Changed the feed to show the posts. Also edited the charts to fit inside of the posts.  Shortened the PieChart file names to make them easier to call.

---
+++ src/components/FeedPost.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './FeedPost.css'
+import PieChart from './PieChart';
+
+
+const FeedPost = () => {
+    return (
+        <div className = "post">
+                <PieChart/>
+        </div>
+    )
+}
+
+export default FeedPost
