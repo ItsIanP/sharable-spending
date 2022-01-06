4b1fcea83a36807fedda57e5446daa16aabf9b3f
Ian Powe <ian.powe@morehouse.edu>
2022-01-06 13:32:02 -0500

    FeedPost
    Please work

---
+++ src/components/FeedPost.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './FeedPost.css'
+import PieChart from './PieChart';
+//feed post
+
+const FeedPost = () => {
+    return (
+        <div className = "post">
+                <PieChart/>
+        </div>
+    )
+}
+
+export default FeedPost
