2eb72985981dcbdb7c49fd1fff543bf286fe0ac5
ryan-31 <ryans03011@gmail.com>
2022-01-06 13:08:58 -0500

    Added default profile picture

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ProfilePic.css'
 
 const ProfilePic = () => {
     const upload = React.useRef(null);
+    const [showPlaceholder, setShowPlaceholder] = React.useState(true)
 
     const imageUpload = e => {
+        setShowPlaceholder(false)
         const [file] = e.target.files;
         if (file) {
             const reader = new FileReader();
@@ -17,12 +19,15 @@ const ProfilePic = () => {
         }
     };
 
-
+    const Placeholder = () => (
+        <img src='https://www.pngarts.com/files/10/Default-Profile-Picture-Free-PNG-Image.png'/>
+    )
     return (
         <div>
             <div className="upload">
-                <input type="file" accept="image/*" onChange={imageUpload} />
-                <img ref={upload} alt="Upload a Profile Picture"/>
+                <input type="file" accept="image/*" onChange={imageUpload}/>
+                {showPlaceholder ? <Placeholder/> : null}
+                <img ref={upload} alt="Default Profile Picture"/>
             </div>
         </div>
     );
