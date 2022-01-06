490403f7f7a05906ce02afb2c4310348ad98b72d
ryan-31 <ryans03011@gmail.com>
2022-01-06 12:25:18 -0500

    Creating & styling profile info

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -22,68 +22,58 @@ const Profile = () => {
                 <div className="acctInfo">
                     <div className="infoHeader">
                         <h2>Account Info</h2>
-                        <p>Double click a field to change information, click anywhere to save</p>
+                        <p>Double click a field to change information, double-click anywhere to save</p>
                     </div>
-                    <div className="username">
-                        <PersonalInfo
-                            text={username}
-                            placeholder="Username"
-                            type="input"
-                        >
-                            <input
-                                type="text"
-                                name="username"
-                                placeholder="New Username"
-                                value={username}
-                                onChange={e => setUsername(e.target.value)}
-                            />
-                        </PersonalInfo>
-                    </div>
-                    <div className="email">
-                        <PersonalInfo
-                            text={email}
-                            placeholder="Email Address"
-                            type="input"
-                        >
-                            <input
-                                type="text"
-                                name="task"
-                                placeholder="Email Address"
-                                value={email}
-                                onChange={e => setEmail(e.target.value)}
-                            />
-                        </PersonalInfo>
-                    </div>
-                    <div className="phone">
-                        <PersonalInfo
-                            text={phone}
-                            placeholder="Phone Number"
-                            type="input"
-                        >
-                            <input
-                                type="text"
-                                name="phone"
-                                placeholder="Phone Number"
-                                value={phone}
-                                onChange={e => setPhone(e.target.value)}
-                            />
-                        </PersonalInfo>
-                    </div>
+                    <div className="fieldGrid">
+                        <div className="username">
+                            <label>Username:</label>
+                            <PersonalInfo text={username} placeholder="Username" type="input">
+                                <input
+                                    type="text"
+                                    name="username"
+                                    placeholder="New Username"
+                                    value={username}
+                                    onChange={e => setUsername(e.target.value)}
+                                />
+                            </PersonalInfo>
+                        </div>
+                        <div className="email">
+                            <label>Email:</label>
+                            <PersonalInfo text={email} placeholder="Email Address" type="input">
+                                <input
+                                    type="text"
+                                    name="task"
+                                    placeholder="Email Address"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                            </PersonalInfo>
+                        </div>
+                        <div className="phone">
+                            <label>Phone Number:</label>
+                            <PersonalInfo text={phone} placeholder="Phone Number" type="input">
+                                <input
+                                    type="text"
+                                    name="phone"
+                                    placeholder="Phone Number"
+                                    value={phone}
+                                    onChange={e => setPhone(e.target.value)}
+                                />
+                            </PersonalInfo>
+                        </div>
 
-                    <div className="address">
-                        <PersonalInfo
-                            text={address}
-                            placeholder="Address"
-                            type="input"
-                        >
-                            <input
-                                type="text"
-                                name="address"
-                                placeholder="Address"
-                                value={address}
-                                onChange={e => setAddress(e.target.value)}
-                            />
-                        </PersonalInfo>
+                        <div className="address">
+                            <label>Address:</label>
+                            <PersonalInfo text={address} placeholder="Address" type="input">
+                                <input
+                                    type="text"
+                                    name="address"
+                                    placeholder="Address"
+                                    value={address}
+                                    onChange={e => setAddress(e.target.value)}
+                                />
+                            </PersonalInfo>
+                        </div>
                     </div>
                 </div>
             </div>
