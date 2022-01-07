3dc6714f9b41d8e6266c707e680ac0b512946ee4
VikasReddy <vikas.red17@gmail.com>
2022-01-07 00:49:51 -0500

    button to feed edit 2

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -2,12 +2,13 @@ import React from 'react'
 import HomePieChart from './HomePieChart'
 import {useNavigate } from 'react-router-dom'
 
-let navigate = useNavigate();
-const feedHandler = (event) => {
-    navigate("../Feed", ({replace: true}));
-}
+
 
 const ShareButton = () => {
+    let navigate = useNavigate();
+    const feedHandler = (event) => {
+        navigate("../Feed", ({replace: true}));
+    }
     return (
         <div>
             <button onClick={feedHandler} style = {{"width": "220px"}}>Share Your Pie Chart!</button>
