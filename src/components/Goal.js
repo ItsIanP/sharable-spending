9863a61f0c71950e60e37e25a0b401e782ddf0d0
Christine Lin <73617227+christinealin7771@users.noreply.github.com>
2022-01-06 23:58:38 -0500

    Finished Goals List

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -8,29 +8,28 @@ var datapts = []
 
 var res = data.map(datas => parseFloat(datas.cost)).reduce((acc, datas) => datas + acc);
 
-function calculate(budget){
-    for (let cat in categories) {
-        var total_per_cost = data.filter(obj => obj.category === categories[cat]).map(datas => parseFloat(datas.cost)).reduce((acc, datas) => datas + acc);
-        console.log(total_per_cost / res, total_per_cost, cat)
-        var percent = ((budget- total_per_cost)/budget) *100
-        datapts.push({y:percent, label: categories[cat]})
-    }
-    return datapts
+for (let cat in categories) {
+    var total_per_cost = data.filter(obj => obj.category === categories[cat]).map(datas => parseFloat(datas.cost)).reduce((acc, datas) => datas + acc);
+    console.log(total_per_cost / res, total_per_cost, cat)
+    var percent = ((500- total_per_cost)/500) *100
+    datapts.push({y:percent, label: categories[cat]})
 }
+
+
 
 
 const Goal = ({category, budget}) => {
     const [random1,  setRandom] = useState(0)
     const [percentage, setPercentage] = useState("")
     
-    for(let cat in calculate(budget)){
-        if(calculate(budget)[cat].label === category){
-            setPercentage(calculate(budget)[cat].y)
-            console.log(percentage)
-        }
+    // for(let cat in calculate(budget)){
+    //     if(calculate(budget)[cat].label === category){
+    //         setPercentage(calculate(budget)[cat].y)
+    //         console.log(percentage)
+    //     }
     
 
-    }
+    // }
 
     
 
@@ -41,7 +40,7 @@ const Goal = ({category, budget}) => {
         <p>{category}</p>
         <p>Spending progress:</p>
         {/* <progress id="file" value = "70" max="100"></progress> */}
-        <progress id="file" value = {calculate(budget)[Math.floor(Math.random() * 7)].y} max="100"></progress>
+        <progress id="file" value = {datapts[Math.floor(Math.random() * 3)].y} max="100"></progress>
 
     </div>
 
